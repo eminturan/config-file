472de79988fa77fecdf1f645472e8af89cdd69d6
Emin Turan <>
2021-05-06 12:31:31 +0300

    deneme

---
+++ libraries/splunk/splunk_step_watcher.groovy
@@ -0,0 +1,14 @@
+@BeforeStep
+void before(){
+  println "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step"
+}
+
+@AfterStep({ config.afterSteps ? (hookContext.step in config.afterSteps) : true })
+void after(){
+  println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
+}
+
+@AfterStep({ currentBuild.result.toString() == "FAILURE" })
+void afterFailure(){
+  println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step failure"
+}
